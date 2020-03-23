c9ef7261a2183babb846377b26ea44ef0151583a
jspeed <jspeed@progenygenetics.com>
2020-03-23 00:28:35 -0700

    website update

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -15,9 +15,11 @@ function AboutPage(props) {
 
             <p>Some Stuff Here</p>
 
-            <p>My latest work was for Progeny Genetics where I worked for almost 3 years.<a href="https://devgrub.com" target="_blank" rel="noopener noreferrer">here</a>, or on the homepage. It is built with Angular, MongoDB, Express JS, and Node JS. However, I will be rebuilding it using React in the coming months</p>
+            <p>Hello, my name is Josiah. I'm a UI/UX designer and software developer.</p>
 
-            <p>Some stuff Here also <a href="http://www.youtube.com/c/GarrettLove1" target="_blank" rel="noopener noreferrer">here</a></p>
+            <p>Currently looking for an ingaging role as a UI/UX designer.</p>
+
+            <p>Some Stuff Here</p>
 
             <p>Hello, my name is Josiah. I'm a UI/UX designer and software developer.</p>
 
@@ -25,39 +27,17 @@ function AboutPage(props) {
 
             <p>Some Stuff Here</p>
 
-            <p>My latest work was for Progeny Genetics where I worked for almost 3 years.<a href="https://devgrub.com" target="_blank" rel="noopener noreferrer">here</a>, or on the homepage. It is built with Angular, MongoDB, Express JS, and Node JS. However, I will be rebuilding it using React in the coming months</p>
-
-            <p>Some stuff Here also <a href="http://www.youtube.com/c/GarrettLove1" target="_blank" rel="noopener noreferrer">here</a></p>
-
             <p>Hello, my name is Josiah. I'm a UI/UX designer and software developer.</p>
 
             <p>Currently looking for an ingaging role as a UI/UX designer.</p>
 
             <p>Some Stuff Here</p>
 
-            <p>My latest work was for Progeny Genetics where I worked for almost 3 years.<a href="https://devgrub.com" target="_blank" rel="noopener noreferrer">here</a>, or on the homepage. It is built with Angular, MongoDB, Express JS, and Node JS. However, I will be rebuilding it using React in the coming months</p>
-
-            <p>Some stuff Here also <a href="http://www.youtube.com/c/GarrettLove1" target="_blank" rel="noopener noreferrer">here</a></p>
-
             <p>Hello, my name is Josiah. I'm a UI/UX designer and software developer.</p>
 
             <p>Currently looking for an ingaging role as a UI/UX designer.</p>
 
             <p>Some Stuff Here</p>
-
-            <p>My latest work was for Progeny Genetics where I worked for almost 3 years.<a href="https://devgrub.com" target="_blank" rel="noopener noreferrer">here</a>, or on the homepage. It is built with Angular, MongoDB, Express JS, and Node JS. However, I will be rebuilding it using React in the coming months</p>
-
-            <p>Some stuff Here also <a href="http://www.youtube.com/c/GarrettLove1" target="_blank" rel="noopener noreferrer">here</a></p>
-
-            <p>Hello, my name is Josiah. I'm a UI/UX designer and software developer.</p>
-
-            <p>Currently looking for an ingaging role as a UI/UX designer.</p>
-
-            <p>Some Stuff Here</p>
-
-            <p>My latest work was for Progeny Genetics where I worked for almost 3 years.<a href="https://devgrub.com" target="_blank" rel="noopener noreferrer">here</a>, or on the homepage. It is built with Angular, MongoDB, Express JS, and Node JS. However, I will be rebuilding it using React in the coming months</p>
-
-            <p>Some stuff Here also <a href="http://www.youtube.com/c/GarrettLove1" target="_blank" rel="noopener noreferrer">here</a></p>
 
             <p>Hello, my name is Josiah. I'm a UI/UX designer and software developer.</p>
 
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -15,9 +15,7 @@ function ContactPage(props) {
 
             <p>Some Stuff Here</p>
 
-            <p>My latest work was for Progeny Genetics where I worked for almost 3 years.<a href="https://devgrub.com" target="_blank" rel="noopener noreferrer">here</a>, or on the homepage. It is built with Angular, MongoDB, Express JS, and Node JS. However, I will be rebuilding it using React in the coming months</p>
-
-            <p>Some stuff Here also <a href="http://www.youtube.com/c/GarrettLove1" target="_blank" rel="noopener noreferrer">here</a></p>
+        
             </Content>
         </div>
     );
